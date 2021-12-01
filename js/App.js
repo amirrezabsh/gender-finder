838c238a85b7ca656eb413748b903e8977759ad6
ammirrezabsh <amirrezabsh79@gmail.com>
2021-12-01 16:58:48 +0330

    bug fix

--- js/App.js
+++ js/App.js
@@ -5,6 +5,10 @@ function submitName() {
         notify(ErrNameMustNotBeEmpty)
         return false
     }
+    if (!validateName(name)) {
+        notify("Enter a valid name!")
+        return false
+    }
     if (localStorage.getItem(name) == null) {
         document.getElementById("answer").innerHTML = "No record found!"
     }
@@ -15,6 +19,13 @@ function submitName() {
     xmlHttp.open( "GET", "https://api.genderize.io/?name=" + name, false ); // false for synchronous request
     xmlHttp.send( null );
     let response = JSON.parse(xmlHttp.response)
+    if (response.gender == null) {
+        notify("No gender found!")
+        document.getElementById("save").disabled = false
+        document.getElementById("gender").innerHTML = "No data!"
+        document.getElementById("score").innerHTML = "No data!"
+        return false
+    }
     document.getElementById("gender").innerHTML = response.gender
     document.getElementById("score").innerHTML = response.probability
     document.getElementById("save").disabled = false
@@ -27,11 +38,15 @@ function saveName(){
         notify(ErrNameMustNotBeEmpty)
         return false
     }
+    if (!document.forms["left-form"]["male-button"].checked && !document.forms["left-form"]["female-button"].checked && document.getElementById("gender").innerHTML == "No data!") {
+        notify("Enter a name and submit")
+        return false
+    }
     if (document.forms["left-form"]["male-button"].checked) {
         localStorage.setItem(name,"Male")
         document.getElementById("answer").innerHTML = "Male"
     }
-    else if (document.forms["left-form"]["female-button"].checked) {
+    else if (document.forms["left-form"]["male-button"].checked) {
         localStorage.setItem(name,"Female")
         document.getElementById("answer").innerHTML = "Female"
     }
@@ -70,3 +85,8 @@ function clear(){
     return false
 }
 document.getElementById("clear").addEventListener("click", clear);
+
+function validateName(name){
+    var regex = /^[a-zA-Z ]{1,255}$/;
+    return regex.test(name);
+}
